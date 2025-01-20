a830765e176ccfe1995a31d6aa87217b5725cdea
Inga Maholwana <imaholwana023@student.wethinkcode.co.za>
2025-01-20 14:18:01 +0200

    zip clean

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,17 +1,3 @@
-# require 'csv'
-# puts 'EventManager initialized.'
-
-# contents = CSV.open(
-#   'event_attendees.csv',
-#   headers: true,
-#   header_converters: :symbol
-# )
-
-# contents.each do |row|
-#   name = row[:first_name]
-#   puts name
-# end
-
 require 'csv'
 puts 'EventManager initialized.'
 
@@ -24,5 +10,10 @@ contents = CSV.open(
 contents.each do |row|
   name = row[:first_name]
   zipcode = row[:zipcode]
+
+  # if the zip code is exactly five digits, assume that it is ok
+  # if the zip code is more than five digits, truncate it to the first five digits
+  # if the zip code is less than five digits, add zeros to the front until it becomes five digits
+
   puts "#{name} #{zipcode}"
-end
+ end
